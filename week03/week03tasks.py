2059df4eb48e51cba8eb7e79bc98b0cd700a6cbb
Ke1ly <o91260210@gmail.com>
2025-02-07 22:03:39 +0800

    week04-v.1

--- week03/week03tasks.py
+++ week03/week03tasks.py
@@ -17,8 +17,8 @@ with request.urlopen(src2) as response2:
 data2=json.loads(data2)
 
 spot_mrt={}
-with open ("spot.csv",mode="w",encoding="utf-8",newline="") as file_spot:
-    writer = csv.writer(file_spot,lineterminator="\n")
+with open ("spot.csv","w",newline="",encoding="utf-8") as file_spot:
+    writer = csv.writer(file_spot, lineterminator='\n')
     for spot in spots:
         url=spot["filelist"].split('https')
         spot["filelist"]="https"+url[1]
@@ -26,9 +26,11 @@ with open ("spot.csv",mode="w",encoding="utf-8",newline="") as file_spot:
             if d["SERIAL_NO"]==spot["SERIAL_NO"]:
                 spot_mrt.update({spot["stitle"]:d["MRT"]})
                 writer.writerow([spot["stitle"],d["address"][5:8],spot["longitude"],spot["latitude"],spot["filelist"]])
-    file_spot.seek(0, os.SEEK_END)
-    file_spot.seek(file_spot.tell()-1, os.SEEK_SET)
-    file_spot.truncate()
+
+    # file_spot.seek(0, os.SEEK_END)
+    # file_spot.seek(file_spot.tell()-1, os.SEEK_SET)
+    # file_spot.truncate()
+
 
 grouped_mrt={}
 for key, value in spot_mrt.items():
@@ -37,65 +39,65 @@ for key, value in spot_mrt.items():
     else:
         grouped_mrt[value].append(key)
 
-with open ("mrt.csv",mode="w",encoding="utf-8",newline="") as file_mrt:
-    writer=csv.writer(file_mrt,lineterminator="\n")
+with open ("mrt.csv",mode="w",newline="",encoding="utf-8") as file_mrt:
+    writer=csv.writer(file_mrt)
     for mrt in grouped_mrt:
         row=[mrt]
         for spot in grouped_mrt[mrt]:
             row.append(spot)
         writer.writerow(row)
-    file_mrt.seek(0, os.SEEK_END)
-    file_mrt.seek(file_mrt.tell()-1, os.SEEK_SET)
-    file_mrt.truncate()
+    # file_mrt.seek(0, os.SEEK_END)
+    # file_mrt.seek(file_mrt.tell()-1, os.SEEK_SET)
+    # file_mrt.truncate()
 
 
 
-# task02
-import urllib.request as req
-import csv
-import bs4
-import os
+# # task02
+# import urllib.request as req
+# import csv
+# import bs4
+# import os
 
-def getTime(url):
-    request=req.Request(url,headers={"Cookie":"over18=1","User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36"})
-    with req.urlopen(request) as response:
-        data=response.read().decode("utf-8")
-    root=bs4.BeautifulSoup(data,"html.parser")
-    time=root.find("span",string="時間")
-    if time != None:
-        time=time.find_next().string
-    else:
-        time=""
-    return time
+# def getTime(url):
+#     request=req.Request(url,headers={"Cookie":"over18=1","User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36"})
+#     with req.urlopen(request) as response:
+#         data=response.read().decode("utf-8")
+#     root=bs4.BeautifulSoup(data,"html.parser")
+#     time=root.find("span",string="時間")
+#     if time != None:
+#         time=time.find_next().string
+#     else:
+#         time=""
+#     return time
 
-def getData(url):
-    request=req.Request(url,headers={"Cookie":"over18=1","User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36"})
-    with req.urlopen(request) as response:
-        data=response.read().decode("utf-8")
-    root=bs4.BeautifulSoup(data,"html.parser")
-    blocks=root.find_all("div",class_="r-ent")
-    with open ("article.csv",mode="a",encoding="utf-8",newline="") as article:
-        writer = csv.writer(article,lineterminator="\n")
-        for block in blocks:
-            title = block.find("div",class_="title")
-            like = block.find("div",class_="nrec")
-            if title.a != None:
-                timeURL="https://www.ptt.cc"+title.a["href"]
-                time = getTime(timeURL)
-                if like.span != None:
-                    writer.writerow([title.a.string,like.span.string,time])
-                else:
-                    writer.writerow([title.a.string,0,time])
-        article.seek(0, os.SEEK_END)
-        article.seek(article.tell()-1, os.SEEK_SET)
-        article.truncate()
+# def getData(url):
+#     request=req.Request(url,headers={"Cookie":"over18=1","User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36"})
+#     with req.urlopen(request) as response:
+#         data=response.read().decode("utf-8")
+#     root=bs4.BeautifulSoup(data,"html.parser")
+#     blocks=root.find_all("div",class_="r-ent")
+#     with open ("article.csv",mode="a",newline="",encoding="utf-8",) as article:
+#         writer = csv.writer(article,lineterminator="\n")
+#         for block in blocks:
+#             title = block.find("div",class_="title")
+#             like = block.find("div",class_="nrec")
+#             if title.a != None:
+#                 timeURL="https://www.ptt.cc"+title.a["href"]
+#                 time = getTime(timeURL)
+#                 if like.span != None:
+#                     writer.writerow([title.a.string,like.span.string,time])
+#                 else:
+#                     writer.writerow([title.a.string,0,time])
+#         # article.seek(0, os.SEEK_END)
+#         # article.seek(article.tell()-1, os.SEEK_SET)
+#         # article.truncate()
             
-    nextpage=root.find("a",string="‹ 上頁")
-    return nextpage["href"]
+#     nextpage=root.find("a",string="‹ 上頁")
+#     return nextpage["href"]
 
-pageURL="https://www.ptt.cc/bbs/Lottery/index.html"
+# pageURL="https://www.ptt.cc/bbs/Lottery/index.html"
 
-count=0
-while count<3:
-    pageURL="https://www.ptt.cc"+getData(pageURL)
-    count+=1
+# count=0
+# while count<3:
+#     pageURL="https://www.ptt.cc"+getData(pageURL)
+#     count+=1
---
+++ week04/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, Request,Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from urllib.parse import quote
+from starlette.middleware.sessions import SessionMiddleware
+
+app=FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
+app.add_middleware(SessionMiddleware, secret_key="jv;rghi;w5ijnm")
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/signin")
+async def signin(request: Request,account: str = Form(...), password: str = Form(...)):
+    if not account or not password:
+        message=quote("Please enter username and password")
+        return RedirectResponse(url=f"/error?message={message}", status_code=303)
+    elif password=="test" and account=="test":
+        request.session["SIGNED-IN"]=True
+        return RedirectResponse(url="/member", status_code=303)
+    else:
+        message=quote("Username or password is not correct")
+        return RedirectResponse(url=f"/error?message={message}", status_code=303)
+
+
+@app.get("/member")
+def memberget(request: Request):
+    if not request.session.get("SIGNED-IN", False) or request.session["SIGNED-IN"]==False:
+        return RedirectResponse(url="/", status_code=303)
+    else:
+        return templates.TemplateResponse("member.html", {"request": request})
+
+@app.get("/error")
+def error(request: Request,message):
+    return templates.TemplateResponse("error.html", {"request": request,"message":message})
+
+@app.get("/square")
+async def square(number, request: Request):
+    number=int(number)
+    return templates.TemplateResponse("square.html",{"request": request,"square":number*number})
+
+@app.get("/signout")
+async def signout(request: Request):
+    request.session.clear()
+    request.session["SIGNED-IN"]=False
+    return RedirectResponse(url="/", status_code=303)
+
